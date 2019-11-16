00b382aba94574dd6919765d0d4626237095e93c
Marcus Pridham <marcuspridham@gmail.com>
2019-11-15 21:06:44 -0500

    Change integration test to use rabbitmq-mock

--- query/src/test/java/com/fombico/query/MessageListenerTest.java
+++ query/src/test/java/com/fombico/query/MessageListenerTest.java
@@ -3,19 +3,17 @@ package com.fombico.query;
 import com.fombico.query.models.QueryDto;
 import com.fombico.query.models.QueryRequest;
 import com.fombico.query.models.QueryResponse;
-import com.fombico.query.utils.EmbeddedInMemoryQpidBrokerRule;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -27,16 +25,14 @@ import static org.springframework.core.ParameterizedTypeReference.forType;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = RabbitTestConfig.class)
 public class MessageListenerTest {
-
-    @ClassRule
-    public static EmbeddedInMemoryQpidBrokerRule qpidBrokerRule = new EmbeddedInMemoryQpidBrokerRule();
-
-    @Value("${qpid.amqp_port}")
-    int amqpPort;
 
     @Autowired
     MessageConverter messageConverter;
+
+    @Autowired
+    ConnectionFactory connectionFactory;
 
     @MockBean
     RabbitTemplate rabbitTemplate;
@@ -80,10 +76,6 @@ public class MessageListenerTest {
     }
 
     private QueryResponse sendMessage(QueryRequest request) {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setHost("localhost");
-        connectionFactory.setPort(amqpPort);
-
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(messageConverter);
         QueryResponse response = template.convertSendAndReceiveAsType(
@@ -91,8 +83,6 @@ public class MessageListenerTest {
                 "storeNumber",
                 request,
                 forType(QueryResponse.class));
-
-        connectionFactory.destroy();
 
         return response;
     }
---
+++ query/src/test/java/com/fombico/query/RabbitTestConfig.java
@@ -0,0 +1,18 @@
+package com.fombico.query;
+
+import com.github.fridujo.rabbitmq.mock.compatibility.MockConnectionFactoryFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class RabbitTestConfig {
+    @Bean
+    ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory(
+                MockConnectionFactoryFactory
+                        .build());
+    }
+}
